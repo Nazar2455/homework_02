3c0fedee07f0bd6fb2dd0801fc75b3e3304c0186
Nazar245 <sobolevskijnazar@gmail.com>
2024-07-23 16:28:12 +0200

    Add files via upload

---
+++ core_homework_04/task_01/my_function.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+current_dir = Path(__file__).parent
+
+def total_salary(path: str):
+    sum_salary = 0
+    count = 0
+    
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()
+            if line:
+                try:
+                    salary = int(line.split(',')[1])
+                    sum_salary += salary
+                    count += 1
+                except (IndexError, ValueError) as e:
+                    print(f"Error processing line: {line} - {e}")
+
+    if count == 0:
+        return "No valid salary data found"
+    
+    avg_salary = sum_salary // count
+    tuple = (sum_salary, avg_salary)
+    return tuple
+
+# relative_path = current_dir / "salary_file.txt"
+# print(total_salary(relative_path))
